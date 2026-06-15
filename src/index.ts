28de232d6cfe328a51fc9e61440ca3f87c8d3a48
dengwei.daviant <dengwei.daviant@jd.com>
2026-06-15 15:40:55 +0800

    feat: add entry point with source registration

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import { registerSource } from './core/registry.js';
+import { createCLI } from './cli.js';
+import { googleNewsSource } from './news-source/google-news/index.js';
+
+// 注册所有新闻源
+registerSource(googleNewsSource);
+
+// 启动 CLI
+const program = createCLI();
+program.parse();
