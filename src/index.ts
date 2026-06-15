8b3cca6bc977f7efc8843a5d497108d5990a9f66
dengwei.daviant <dengwei.daviant@jd.com>
2026-06-15 16:43:01 +0800

    feat: add google-news-cn source (Chinese edition)
    - Reuse parser from google-news
    - Add hl=zh-CN&gl=CN&ceid=CN:zh-Hans locale params to all URLs
    - Same 8 categories + keyword search
    - 5 unit tests

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { registerSource } from './core/registry.js';
 import { createCLI } from './cli.js';
 import { googleNewsSource } from './news-source/google-news/index.js';
+import { googleNewsCNSource } from './news-source/google-news-cn/index.js';
 
 // 注册所有新闻源
 registerSource(googleNewsSource);
+registerSource(googleNewsCNSource);
 
 // 启动 CLI
 const program = createCLI();
