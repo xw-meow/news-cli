e401e3c43573d8f5474fccb2ccadc0755b83790e
dengwei.daviant <dengwei.daviant@jd.com>
2026-06-17 17:07:34 +0800

    feat(site): add Footer

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,15 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import { Header } from './components/layout/Header';
+import { Footer } from './components/layout/Footer';
 
 export function App() {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="pt-14">
+      <main className="pt-14 flex-1">
         <Routes>
           <Route path="/" element={<div className="p-8 text-green-400 font-mono">news-cli</div>} />
         </Routes>
       </main>
-    </>
+      <Footer />
+    </div>
   );
 }
