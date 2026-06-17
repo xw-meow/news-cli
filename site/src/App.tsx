299997193fc9df3f6a6d6f6313c52d98fab97b20
dengwei.daviant <dengwei.daviant@jd.com>
2026-06-17 17:06:41 +0800

    feat(site): add Header with mobile hamburger menu

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,9 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
+import { Header } from './components/layout/Header';
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<div className="p-8 text-green-400 font-mono">news-cli</div>} />
-    </Routes>
+    <>
+      <Header />
+      <main className="pt-14">
+        <Routes>
+          <Route path="/" element={<div className="p-8 text-green-400 font-mono">news-cli</div>} />
+        </Routes>
+      </main>
+    </>
   );
 }
